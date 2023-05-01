e80210d06901f66548a6348ab47eb2863631f714
Rajesh Kumar <rajkuma@L-HYDQ9Z4MD6M-M.local>
2023-05-01 23:56:38 +0530

    [RK]: Added boiler plate code for sorting techniques react app

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Sorting Techniques
+      </header>
+      <div className="App-body">
+        <a href="./BubbleSort.js">Bubble Sort</a>
+        <a href="./SelectionSort.js">Selection Sort</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
